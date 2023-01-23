9f2d7d80e5c1cc28eacbf76ce85d0e8026c56a6f
Bulgarsky <bulgarpepper@gmail.com>
2023-01-23 11:55:47 +0500

    add pratice02 file

---
+++ University_Fullstack/Backend (Java)/Plehanov/practice_02_calc/Calc.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Calc {
+    public static void main(String[] args) {
+        double result;
+        //a = 2;
+        //b = 3;
+        System.out.println("Arithmetic operation (use comma) ");
+        System.out.print("enter A = ");
+        Scanner in = new Scanner(System.in);
+        double a = in.nextDouble();
+        //Scanner in = new Scanner(System.in);
+        System.out.print("enter B = ");
+        double b = in.nextDouble();
+
+        System.out.printf("entered data: A = %.2f; B = %.2f \n", a, b);
+        
+        result = a + b;
+        System.out.printf("sum(%.2f + %.2f) = %.2f \n", a, b, result);
+        
+        result = a - b;
+        System.out.printf("difference (%.2f + %.2f)  = %.2f \n", a, b, result);
+
+        result = a * b;
+        System.out.printf("multiplication (%.2f * %.2f) = %.2f \n", a, b, result);
+
+        result = a / b;
+        System.out.printf("division (%.2f / %.2f) = %.2f \n", a, b, result);
+    }
+}
