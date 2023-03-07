de677478b3564b94a5d8b96ed47e31f883d245ec
Bulgarsky <bulgarpepper@gmail.com>
2023-03-07 09:01:43 +0500

    j 11 practice add for()

--- University_Fullstack/Backend (Java)/practice_11_collection/src/Main.java
+++ University_Fullstack/Backend (Java)/practice_11_collection/src/Main.java
@@ -5,23 +5,35 @@ public class Main {
         Set<Integer> numerico = new TreeSet<>();
         System.out.println("input number(int): ");
         Scanner in = new Scanner(System.in);
+        //бесконечный цикл
         for (int i=0; true; i++) {
             int userInput = in.nextInt();
             if (!numerico.contains(userInput)) { //== false
                 numerico.add(userInput);
             } else {
+                //вывoд дубликата
                 System.out.println("duplicate: "+ userInput);
-                System.out.println("numerico: "+numerico);
+                // вывод [размер] элементы
+                System.out.println("numerico["+numerico.size()+"]: "+numerico);
+                //переборка и вывод элементов по отдельности
+                for (int element: numerico) {
+                    System.out.println(element);
+                }
+                //сообщение о необходимости удаления
                 System.out.println("input number from "+numerico+" to delete");
-                break;
+                break; // прерывание цикла в случае перехода в else
             }
         }
+        //получение числа для удаления
         int delete = in.nextInt();
+        //сравнение
         if (numerico.contains(delete)) { //== true
             numerico.remove(delete);
-            System.out.println("deleted: "+delete);
-            System.out.println("updated [numerico] is "+numerico);
+            System.out.println("deleted: "+delete); //вывод удаленног числа
+            //вывод обновленного набора
+            System.out.println("updated numerico["+numerico.size()+"] is "+numerico);
         } else {
+            //вывод информации об отсутствии
             System.out.println("element: "+delete+" not found in "+numerico);
         }
     }
---
+++ University_Fullstack/Backend (Java)/practice_12_airport/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("AIRPORT");]
+        //start  - input details
+       // flightDetails () {}
+        //flightNo AA1234 check @override equals()
+        //airportName
+        //carrierName
+        //destination
+        //departTime
+        //ticketPrice
+        //repeat if flightNo ! {AA1234}A
+        //1 - submit
+        //2 - printAllflights
+        //3 - exit
+    }
+}
