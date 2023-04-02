6811816cb717dade4ead2befd0e40483d825d2b8
Bulgarsky <bulgarpepper@gmail.com>
2023-04-02 14:42:00 +0500

    crud, web, valid, need refactor

---
+++ University_Fullstack/Java Spring/spring-13_CRUD_product_postgres/src/main/java/com/example/products/contollers/Product.java
@@ -0,0 +1,88 @@
+package com.example.products.contollers;
+
+import com.example.products.dao.DaoProduct;
+import com.example.products.enumm.Provider;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+public class Product {
+    private final DaoProduct daoProduct;
+
+    public Product(DaoProduct daoProduct) {
+        this.daoProduct = daoProduct;
+    }
+
+    //product/add?name=Молоко&price=170&weight=2&provider=agroprom
+    //product/add?name=Хлеб&price=40&weight=1&provider=bars
+
+
+//    @GetMapping("/product/add") //get param and addProduct to ArrayList DAO
+//    public void addProduct(
+//            @RequestParam(value = "name", required = false) String name,
+//            @RequestParam(value = "price", required = false) float price,
+//            @RequestParam(value = "weight", required = false) String weight,
+//            @RequestParam(value = "provider", required = false) Provider provider){
+//        daoProduct.addProduct(name, price, weight, provider);
+//    }
+
+
+    @GetMapping("/product") //get productList and return view/#
+    public String index(Model model) {
+        model.addAttribute("product", daoProduct.getProductList());
+        return "product";
+    }
+
+    @GetMapping("/product/{id}")
+    public String productInfo (@PathVariable("id") int id, Model model) {
+        model.addAttribute("product", daoProduct.getProductId(id));
+        return "product_info";
+    }
+
+    @GetMapping("/product/add") //form view
+    public String addProduct(Model model) {
+        model.addAttribute("product", new com.example.products.models.Product());
+        return "add_product";
+    }
+
+    @PostMapping("/product/add") //submit form and safe product
+    public String newProduct(
+            @ModelAttribute("product")
+            @Valid com.example.products.models.Product product,
+            BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "add_product";
+        }
+        daoProduct.addProduct(product);
+        return "redirect:/product";
+    }
+
+    @GetMapping("/product/edit/{id}") //get obj(id) for redaction and return form view
+    public String editProduct(@PathVariable("id") int id, Model model) {
+        model.addAttribute("editProduct", daoProduct.getProductId(id));
+        return "edit_product";
+    }
+
+    @PostMapping("/product/edit/{id}") //get obj(id) for redaction and return form view
+    public String updateEditProduct(
+            @ModelAttribute("editProduct")
+            @Valid com.example.products.models.Product product,
+            BindingResult bindingResult,
+            @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "edit_product";
+        }
+        daoProduct.updateProduct(id, product);
+        return "redirect:/product";
+    }
+
+    @GetMapping("product/delete/{id}")
+    public String deleteProduct(@PathVariable("id") int id) {
+        daoProduct.removeProduct(id);
+        return "redirect:/product";
+    }
+}
---
+++ University_Fullstack/Java Spring/spring-13_CRUD_product_postgres/src/main/java/com/example/products/dao/DaoProduct.java
@@ -0,0 +1,53 @@
+package com.example.products.dao;
+
+import com.example.products.enumm.Provider;
+import com.example.products.models.Product;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class DaoProduct {
+    private int id;
+    private List<Product> productList = new ArrayList<>();
+
+    // create obj and add to ArrayList
+//    public void addProduct(String name, float price, String weight, Provider provider) {
+//        productList.add(new Product(++id, name, price, weight, provider));
+//    }
+    public void addProduct(Product product) {
+        productList.add(product);
+        product.setId(++id);
+    }
+
+    //getter: all product from ArrayList
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public Product getProductId(int id) {
+        for (Product item: productList) {
+            if(item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
+        //lambda
+        //return productList.stream().filter(product -> product.getId() == id).findAny().orElse(null);
+    }
+
+    //найти товар по id и внести изменения полученные из формы
+    public void updateProduct(int id, Product product) {
+        Product productUpdate = getProductId(id);
+        productUpdate.setName(product.getName());
+        productUpdate.setPrice(product.getPrice());
+        productUpdate.setWeight(product.getWeight());
+        productUpdate.setProvider(product.getProvider());
+    }
+
+    //удаление
+    public void removeProduct(int id) {
+        productList.removeIf((product -> product.getId() == id));
+    }
+}
