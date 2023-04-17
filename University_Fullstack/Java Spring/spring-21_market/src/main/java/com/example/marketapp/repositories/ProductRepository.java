b606f1379fffdae958caa5c01443601d54fab819
Bulgarsky <bulgarpepper@gmail.com>
2023-04-17 23:24:36 +0500

    add: product service

--- University_Fullstack/Java Spring/spring-21_market/src/main/java/com/example/marketapp/repositories/ProductRepository.java
+++ University_Fullstack/Java Spring/spring-21_market/src/main/java/com/example/marketapp/repositories/ProductRepository.java
@@ -1,4 +1,10 @@
 package com.example.marketapp.repositories;
 
-public interface ProductRepository {
+import com.example.marketapp.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
 }
---
+++ University_Fullstack/Java Spring/spring-21_market/src/main/java/com/example/marketapp/services/ProductService.java
@@ -0,0 +1,49 @@
+package com.example.marketapp.services;
+
+
+import com.example.marketapp.models.Category;
+import com.example.marketapp.models.Product;
+import com.example.marketapp.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    //m getback all products
+    public List<Product> getAllProduct(){
+        return productRepository.findAll();
+    }
+    //m getback product w/ id
+    public Product getProductId(int id){
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        return optionalProduct.orElse(null);
+    }
+
+    //m save product w category
+    @Transactional
+    public void saveProduct(Product product, Category category){
+        product.setCategory(category);
+        productRepository.save(product);
+    }
+
+    //m update product info
+    public void updateProduct(int id, Product product){
+        product.setId(id);
+        productRepository.save(product);
+    }
+    //m delete product w id
+    @Transactional
+    public void deleteProduct(int id){
+        productRepository.deleteById(id);
+    }
+}
