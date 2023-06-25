23764c16ae7c3c834dcf04d3c5a5db38b4894d5f
Bulgarsky <bulgarpepper@gmail.com>
2023-06-25 19:33:19 +0500

    webpack lf

---
+++ webpack/src/some.js
@@ -0,0 +1,17 @@
+//private
+function sum(...numbers){
+    let sum = 0;
+
+    for (let i = 0; i < numbers.length; i++){
+        sum += numbers[i];
+    }
+
+    return sum;
+}
+
+//public
+function avg(...numbers) {
+    return sum(...numbers)/numbers.length;
+}
+
+export default avg;
