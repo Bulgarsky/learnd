fa58492f17bce801561bd32fa08ee3513116fa64
Bulgarsky <bulgarpepper@gmail.com>
2023-06-29 01:36:49 +0500

    backup

--- react/src/MinMax.js
+++ react/src/MinMax.js
@@ -1,15 +1,22 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import {prototype} from "mini-css-extract-plugin";
 
+Counter.prototype = {
+    min: PropTypes.number,
+    max: PropTypes.number.isRequired,
+    current: PropTypes.number.isRequired
+}
 function Counter({ min= 1, max }){
-    let [current, setCurrent] = useState(min);
+
     console.log(current);
 
     function validateDiapason(num){
        let validateNum = Math.max(min, Math.min(max, num));
-       setCurrent(validateNum);
+      // setCurrent(validateNum);
+        console.log('upd.');
     }
 
     //Если границы двух значные onChange багуется
@@ -21,26 +28,15 @@ function Counter({ min= 1, max }){
     let increment = () => validateDiapason(current + 1);
     let decrement = () => validateDiapason(current - 1);
 
+
     /*
-    function increment(){
-        if (current < max) {
-            setCurrent(current + 1);
-        }
-    }
-
-    function decrement(){
-        if (current > min) {
-            setCurrent(current - 1);
-        }
-    }
-     */
-
     //если задать пустой массив [] - то сработает раз
     //можно передать параметры на какие хотим смотреть, мульт вотчер
     useEffect(() => {
         console.log('echo useEffect');
         validateDiapason(current);
     }, [min, max]);
+     */
 
 
     return (
@@ -55,10 +51,7 @@ function Counter({ min= 1, max }){
     )
 }
 
-Counter.prototype = {
-    min: PropTypes.number,
-    max: PropTypes.number.isRequired
-}
+
 
 Counter.defaultProps = {}
 
