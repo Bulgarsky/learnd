be1675adf2627e19656b91ab53425f5250e43490
Bulgarsky <bulgarpepper@gmail.com>
2024-01-11 16:54:15 +0500

    gitignore

--- University_Fullstack/ReactJS/react-10-nestjs/my-nest/dist/product.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.product = void 0;
-const product_model_1 = require("./product.model");
-exports.product = [
-    new product_model_1.Product(1, 'notebook', 55000, 'gaming notebook')
-];
-//# sourceMappingURL=product.js.map
--- University_Fullstack/ReactJS/react-10-nestjs/my-nest/dist/product.model.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Product {
-    id: number;
-    title: string;
-    price: number;
-    description: string;
-    constructor(id: number, title: string, price: number, description: string);
-}
